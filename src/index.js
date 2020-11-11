5f49ccc2844404d2e2b223f0a9db1259bd2fce61
Natã Rodrigues <nata.18rodrigues@gmail.com>
2020-11-11 08:57:49 -0300

    finalização com react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const List = () => {
+/**
+ * Lista de atividade com props (nivel médio)
+ * 
+ * Passe a prop IDADE como um número[ok]
+ * Passe uma prop chamada times que seja um array ["palmeiras", "santos", "são paulo"][]
+ * Passe uma prop chamada ligado que seja um boolean (se ligado for verdadeiro então deve aparecer a mensagem "Ligado" se não "Desligado")
+ */
 
+const MostrarNomes = (props) => {
+  return (
+    <div>
+      <h1> {props.nome}  {props.idade} </h1>
+      <h3>{props.estado == true ? 'ligado' : 'desligado'}</h3>
+
+      {props.fifa.map(item => {
+        return (
+          <li> {item} </li> 
+        )
+      })}
+
+      {props.usuario.email}
+    </div> 
+  );
+}
+
+
+
+const List = () => {
   const [texto, setTexto] = React.useState("");
   const [lista, setLista] = React.useState([]);
 
@@ -65,6 +91,29 @@ const List = () => {
           );
         })}
       </ul>
+
+
+
+      <MostrarNomes 
+        usuario={{ nome: 'Natan', email: 'natan@gmail.com' }} 
+        fifa={[]} 
+        nome="Matheus Paice com prop" 
+        idade={22} 
+        estado={false}>
+      </MostrarNomes>
+
+      <MostrarNomes usuario={
+        { nome: 'Matheus', email: 'matheus@gmail.com' }
+      } fifa={[]} nome="Natan Rodrigues com prop" idade={18} estado={false}></MostrarNomes>
+      <MostrarNomes usuario={
+        { nome: 'Lucas', email: 'lucas@gmaailc.om' }
+      } fifa={["palmeiras", "santos", "são paulo"]}></MostrarNomes>
+      <MostrarNomes usuario={
+        { nome: 'Daniel', email: 'daniel@gmail.com' }
+      } fifa={[]} estado={false}></MostrarNomes>
+      <MostrarNomes usuario={
+        { nome: 'Poli', email: 'poli@gmail.com' }
+      } fifa={[]} estado={true}></MostrarNomes>
     </div>
   )
 }
